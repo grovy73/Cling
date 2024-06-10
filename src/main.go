a4dc97c2665fcb6cb12d8197013143c8ad1619d3
grovy73 <holger.bregendahl@hotmail.com>
2024-06-10 20:30:42 +0200

    added flags

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -10,6 +11,12 @@ type NoClingGuide struct{}
 
 func (m *NoClingGuide) Error() string {
 	return ".cpp file does not contain // CLING GUIDE"
+}
+
+type NoLibs struct{}
+
+func (m *NoLibs) Error() string {
+	return "Couldn't find any libs"
 }
 
 func check_err(e error) {
@@ -33,19 +40,37 @@ func find_libs(content []byte, line_num int) ([]string, error) {
 	if libs := strings.Split(lines[line_num], " "); libs[1] == "libs" {
 		return libs[2:], nil
 	}
-	return nil, &NoClingGuide{}
+	return nil, &NoLibs{}
+}
+
+func add_libs_to_cmd(cmd string, libs []string) string {
+	for _, lib := range libs {
+		cmd = fmt.Sprintf("%s -l%s", cmd, lib)
+	}
+	return cmd
 }
 
 func main() {
-	data, err := os.ReadFile("test.cpp")
+	output_name := flag.String("o", "main", "used to change output file name")
+	flag.Parse()
+
+	if flag.NArg() < 1 {
+		fmt.Println("Incorrect usage. Correct usage:\n\tcling example.cpp")
+		return
+	}
+
+	cmd := fmt.Sprintf("clang++ %s -o %s", flag.Arg(0), *output_name)
+
+	data, err := os.ReadFile(flag.Arg(0))
 	check_err(err)
 
 	if line_num, err := contains_cling(data); err == nil {
 		libs, err := find_libs(data, line_num)
 		check_err(err)
-		fmt.Println(libs)
+		cmd = add_libs_to_cmd(cmd, libs)
 	} else {
 		panic(err)
 	}
 
+	fmt.Println(cmd)
 }
